7db91ec19c9a16d231c1e860d2ffcefb0b32527a
vrsmarcos00 <vrr.marcos@gmail.com>
2021-09-24 15:45:43 -0300

    complected project

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,47 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Account;
+import model.exceptions.DomainException;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 		
+		try {
+			System.out.println("Enter account data: ");
+			System.out.print("Number: ");
+			int number = sc.nextInt();
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.print("Initial balance: ");
+			Double initialBalance = sc.nextDouble();
+			System.out.print("Withdraw limit: ");
+			Double withdrawLimit = sc.nextDouble();
+			
+			Account account = new Account(number, name, initialBalance, withdrawLimit);
+			
+			System.out.println();
+			
+			System.out.print("Enter amount for withdraw: ");
+			Double whitdraw = sc.nextDouble();
+			account.withdraw(whitdraw);
+			
+			System.out.println("New Balance: " + String.format("%.2f", account.getBalance()));
+		}
+		catch (DomainException e) {
+			System.out.println("Withdraw error: " + e.getMessage());
+		}
+		
+		
+		
+		sc.close();
 
 	}
 
